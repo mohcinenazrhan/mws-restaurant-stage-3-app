fd4a5e4e1c3f744c9e4abdcaf9031452806015ee
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2018-12-29 12:03:38 +0000

    fix: support Foreach over Nodelist in Ie11 - create polyfill function in functions script and call it when need it - call polyfillIe11NodelistForeach func when post review

--- src/js/functions.js
+++ src/js/functions.js
@@ -114,3 +114,18 @@ export function appendPolyfill() {
         });
     })([Element.prototype, Document.prototype, DocumentFragment.prototype]);
 }
+
+/**
+ * Polyfill ie 11 nodelist foreach
+ */
+export function polyfillIe11NodelistForeach() {
+    if ('NodeList' in window && !NodeList.prototype.forEach) {
+        console.info('polyfill for IE11');
+        NodeList.prototype.forEach = function (callback, thisArg) {
+            thisArg = thisArg || window;
+            for (var i = 0; i < this.length; i++) {
+                callback.call(thisArg, this[i], i, this);
+            }
+        };
+    }
+}
